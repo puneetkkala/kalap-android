50216cb673ca79536856c6ad37ef30ce52981199
Puneet Kala <puneetkkala@gmail.com>
2015-12-09 00:35:14 +0530

    Adding CustomNotification
    Demo on how to create custom notifications

---
+++ CustomNotificationDemo/java/com/kalapuneet/notification/MainActivity.java
@@ -0,0 +1,47 @@
+package com.kalapuneet.notification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    NotificationCompat.Builder notification;
+    //We need id number of each notification we give
+    private static final int uniqueId = 7413;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+    }
+
+    public void notifyClick(View view){
+        //Build the notification
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        notification.setTicker("This is the ticker");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Here is the title");
+        notification.setContentText("I am the body text of your notification");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Builds notification and issues it
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueId, notification.build());
+    }
+
+
+
+
+}
